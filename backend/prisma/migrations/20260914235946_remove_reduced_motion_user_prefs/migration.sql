aa00a9533ec08fe0ebc40fd3cc9056a9a7cfa6b6
Carlos Verissimo <carlosverissimo3001@gmail.com>
2026-09-15 01:00:24 +0100

    chore(schema): drop reduced_motion

---
+++ backend/prisma/migrations/20260914235946_remove_reduced_motion_user_prefs/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `reduced_motion` on the `user_preferences` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "user_preferences" DROP COLUMN "reduced_motion";
